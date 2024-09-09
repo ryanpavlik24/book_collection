c62bf7ea88f6997d0b5395d9ee3352c4c2e74808
ryanpavlik24 <ryan.pavlik@tamu.edu>
2024-09-09 22:55:03 +0000

    Book Collection 4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_05_162758) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_09_215651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,19 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_05_162758) do
     t.string "author"
     t.integer "price"
     t.date "publishedDate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_books", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
